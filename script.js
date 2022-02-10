9406dc84415cc95ab9652ae1d0ca5b6b79b16430
David Stobbe <d.stobbe1@share.epsb.ca>
2022-02-10 13:28:38 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Average Calculator - David
+
+// Input
+let num1 = +prompt("Enter first number:");
+let num2 = +prompt("Enter second number:");
+let num3 = +prompt("Enter third number:");
+
+
+// Process
+let average = (num1 + num2 + num3) / 3;
+
+// Output
+alert("The average of the numbers is: " + average);
